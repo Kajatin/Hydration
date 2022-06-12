765e7f2dd28cbda67a1842c99f4bd25ffdb2fec1
Roland Kajatin <roland.kajatin@gmail.com>
2022-06-12 12:27:11 +0200

    Initial Commit

---
+++ Hydration Watch App/HydrationApp.swift
@@ -0,0 +1,19 @@
+//
+//  HydrationApp.swift
+//  Hydration Watch App
+//
+//  Created by Roland Kajatin on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hydration_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
